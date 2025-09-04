f65257a65772c8d0096d593dcfb2c04575cfff43
Ryu Higashi <ryuhigashi@hotmail.com>
2025-09-04 17:36:57 -0300

    start jogo

---
+++ modulo_2/projeto_jogo/jogo.py
@@ -0,0 +1,60 @@
+# Personagem: Classe mãe
+# Heroi: herda tudo da classe mãe da classe personagem - Controlado pelo usuario
+# Inimigo: Classe do adversario
+
+
+
+
+class Personagem:
+    def __init__(self, nome, vida, nivel):
+        self.__nome = nome
+        self.__vida = vida
+        self.__nivel = nivel
+
+
+    def get_nome(self):
+        return self.__nome
+    
+    def get_nivel(self):
+        return self.__nivel
+    
+    def get_vida(self):
+        return self.__vida
+    
+    def exibir_detalhes(self):
+        return f"Nome: {self.get_nome()}\nVida: {self.get_vida()}\nNivel: {self.get_nivel()}"
+    
+
+class Heroi(Personagem):
+    def __init__(self, nome, vida, nivel, habilidade):
+        super().__init__(nome, vida, nivel)
+        self.__habilidade = habilidade
+
+    def get_habilidade(self):
+        return self.__habilidade
+    
+    def exibir_detalhes(self):
+        return f"{super().exibir_detalhes()}\nHabilidade: {self.get_habilidade()}\n"
+    
+    
+
+class Inimigo(Personagem):
+    def __init__(self, nome, vida, nivel, tipo):
+        super().__init__(nome, vida, nivel)
+        self.__tipo = tipo
+
+    def get_tipo(self):
+        return self.__tipo
+    
+    def exibir_detalhes(self):
+        return f"{super().exibir_detalhes()}\nTipo: {self.get_tipo()}\n"
+
+
+
+heroi = Heroi(nome="Herói", vida=100, nivel=5, habilidade="super-força")
+print(heroi.exibir_detalhes())
+inimigo = Inimigo(nome="Inimigo", vida=50, nivel=3, tipo="voador")
+print(inimigo.exibir_detalhes())
+
+
+
